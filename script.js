f1800635b3e72f2a9db0942a74bc6a9dfc7788d6
George Bastawros <george.bastawros@outlook.com>
2022-09-08 19:14:20 -0500

    built up hangman cancas

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function alphabetpad() {
     for (let i = 0; i < alphabet.length; i++) {
         letterButtons = document.createElement('button');
         letterButtons.innerHTML = alphabet[i];
+        letterButtons.onclick = play()
         document.getElementById('alphabet').appendChild(letterButtons);
     }
 }
@@ -32,7 +33,7 @@ function startPlay() {
             }
             let dashes = wordToPlay(word)
 
-            document.getElementById('playWord').innerHTML = word
+            document.getElementById('playWord').innerHTML = dashes
 
         })
         document.getElementById('categotries').appendChild(categoryBtn)
@@ -42,7 +43,98 @@ function startPlay() {
 }
 //Convert the rundamly generated word to dashes//
 function wordToPlay(word) {
-    console.log(word)
+    let secretArray = [];
+    for (let j = 0; j < word.length; j++) {
+        secretArray.push('-')
+    }
+    return secretArray.join(' ')
+}
+
+//drawing hangman//
+function baseBar() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(10, 290);
+    ctx.lineTo(20, 290);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function vertBar() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(15, 290);
+    ctx.lineTo(15, 190);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function horBar() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(15, 190);
+    ctx.lineTo(40, 190);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function rod() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(40, 190);
+    ctx.lineTo(40, 185);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function head() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.begiPath();
+    ctx.arc(40, 175, 10, 0, 2 * Math.PI);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function body() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(40, 165);
+    ctx.lineTo(40, 135);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function leftArm() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(40, 160);
+    ctx.lineTo(35, 155);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function rightArm() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(40, 160);
+    ctx.lineTo(45, 155);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function leftLeg() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(40, 135);
+    ctx.lineTo(35, 130);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+function rightLeg() {
+    const element = document.getElementById('hangman');
+    ctx = element.getContext("2d");
+    ctx.moveTo(40, 135);
+    ctx.lineTo(45, 130);
+    ctx.lineWidth = 10;
+    ctx.stroke();
+}
+var hangmanArray = [baseBar, vertBar, horBar, rod, head, body, leftArm, rightArm, leftLeg, rightLeg]
+
+function play() {
+
 }
 startPlay()
 
