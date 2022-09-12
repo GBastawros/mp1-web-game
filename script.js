ff823b91adec54c1cf3c2f1d360064a43fe5407e
George Bastawros <george.bastawros@outlook.com>
2022-09-11 21:07:29 -0500

    testing words

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 // Create alphabet pad //
-
+var letters = document.getElementById('alphabet');
 function alphabetpad() {
     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+    
     for (let i = 0; i < alphabet.length; i++) {
-        letterButtons = document.createElement('button');
+        let letterButtons = document.createElement('button');
         letterButtons.innerHTML = alphabet[i];
-        document.getElementById('alphabet').appendChild(letterButtons);
+        letterButtons.addEventListener('click', function () {
+            for (let i = 0; i < wordArray.length; i++) {
+                if (wordArray[i] === letterButtons.innerHTML) {
+                    console.log(dashes)
+                   
+                //     let x = document.getElementById('playWord').innerHTML
+                //     let y = x.split('') 
+                //     console.log(y)
+                //     y.splice(y[i],1,wordArray[i]);
+                //     // y[i] = wordArray[i]
+                // //    x[i] = letterButtons.innerHTML
+                   console.log(i)
+                   console.log (letterButtons.innerHTML)
+                }
+            }
+        })
+
+        letters.appendChild(letterButtons);
     }
 }
 alphabetpad()
-
 // Start Play by selecting a category to create a random word//
 const cars = ['chevrolet', 'ford', 'mercedes', 'subaru', 'toyota'];
 const cities = ['madison', 'chicago', 'cairo', 'manchester'];
 const fruits = ['banana', 'mango', 'apple']
 const categoriesPad = ["cars", "cities", "fruits"];
+// const categories = [cars, cities, fruits];
 
-function startPlay() {
-    let word;
+let word;
+let categoriesDiv = document.getElementById('categotries')
+let playWordP = document.getElementById('playWord')
     for (let i = 0; i < categoriesPad.length; i++) {
         let categoryBtn = document.createElement("button")
         categoryBtn.innerHTML = categoriesPad[i]
@@ -31,26 +49,51 @@ function startPlay() {
             } else if (categoryBtn.innerHTML === "fruits") {
                 word = fruits[Math.floor(Math.random() * fruits.length)]
             }
-            //console.log(word)
-            let dashes = wordToPlay(word)
-            document.getElementById('playWord').innerHTML = dashes
+           console.log(word)
+             let dashes = wordToPlay(word)
+            playWordP.innerHTML = dashes.join('')
         })
-        document.getElementById('categotries').appendChild(categoryBtn)
-
+        categoriesDiv.appendChild(categoryBtn)
     }
-    return word;
-}
-startPlay()
+
+
+
+
+// function startPlay() {
+//     let word;
+//     for (let i = 0; i < categoriesPad.length; i++) {
+//         let categoryBtn = document.createElement("button")
+//         categoryBtn.innerHTML = categoriesPad[i]
+//         categoryBtn.addEventListener('click', function () {
+//             if (categoryBtn.innerHTML === "cars") {
+//                 word = cars[Math.floor(Math.random() * cars.length)]
+//             } else if (categoryBtn.innerHTML === "cities") {
+//                 word = cities[Math.floor(Math.random() * cities.length)]
+//             } else if (categoryBtn.innerHTML === "fruits") {
+//                 word = fruits[Math.floor(Math.random() * fruits.length)]
+//             }
+//             console.log(word)
+//             let dashes = wordToPlay(word)
+//             document.getElementById('playWord').innerHTML = dashes.join('')
+//         })
+//         document.getElementById('categotries').appendChild(categoryBtn)
+
+//     }
+//     return word;
+// }
+// startPlay()
+
 
 //Convert the rundamly generated word to dashes//
-let wordArray;
+let wordArray = [];
+
 function wordToPlay(word) {
     wordArray = word.split('')
     let secretArray = [];
     for (let j = 0; j < word.length; j++) {
         secretArray.push('-')
     }
-    return secretArray.join(' ')
+    return secretArray
 }
 
 //drawing hangman//
@@ -90,7 +133,7 @@ function head() {
     const element = document.getElementById('hangman');
     ctx = element.getContext("2d");
     ctx.beginPath();
-    ctx.arc(148,45,15,0,2 * Math.PI);
+    ctx.arc(148, 45, 15, 0, 2 * Math.PI);
     ctx.stroke();
 }
 function body() {
@@ -134,14 +177,13 @@ function rightLeg() {
     ctx.stroke();
 }
 const hangmanArray = [baseBar, vertBar, horBar, rod, head, body, leftArm, rightArm, leftLeg, rightLeg];
-// hangman[0]()
-// hangman[1]()
-// hangman[2]()
-// hangman[3]()
-// hangman[4]()
-// hangman[5]()
-// hangman[6]()
-// hangman[7]()
-// hangman[8]()
-// hangman[9]()
-
+// hangmanArray[0]()
+// hangmanArray[1]()
+// hangmanArray[2]()
+// hangmanArray[3]()
+// hangmanArray[4]()
+// hangmanArray[5]()
+// hangmanArray[6]()
+// hangmanArray[7]()
+// hangmanArray[8]()
+// hangmanArray[9]()
